ddd92e1a0d9a9e91eb488742a1b2a904232dd754
MokiEng <meconen20@gmail.com>
2023-05-16 09:27:27 -0700

    List by best

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,7 +1,6 @@
 -- a script that inserts a new row in the table first_table,
 -- (database hbtn_0c_0) in your MySQL server.
 -- with New row:
---id = 89
+-- id = 89
 -- name = Best School
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
-SELECT COUNT(*) FROM first_table WHERE id = 89;
+INSERT INTO first_table (id, name) VALUES (89, 'BestSchool');
