1d163d7cf130eb8bb7f28a08b245f07977bb1ad6
MokiEng <meconen20@gmail.com>
2023-05-17 08:22:25 -0700

    Genre ID

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows 
+-- that have at least one genre linked.
+-- You can use only one SELECT statement
+-- the database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
