0b8b0960b2c45491ba557aaec25729e9ddd70617
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-13 11:42:12 -0700

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,7 +4,7 @@ exports.nbOccurrences = function (list, searchElement) {
   let count = 0;
 
   for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
+    if (searchElement === list[i]) {
       count++;
     }
   }
