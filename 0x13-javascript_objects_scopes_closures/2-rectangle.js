81b4ca889250f3c5486de49e6a44fed1a7c08c8f
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-13 07:46:58 -0700

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,9 @@
 
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       /* Create an empty object if width or height is invalid */
+      return {};
     }
 
     this.width = w;
