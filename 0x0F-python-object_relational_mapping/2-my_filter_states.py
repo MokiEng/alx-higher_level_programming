5ff7922cb5590dc6aeadffdc20c6443ef50d58bd
MokiEng <meconen20@gmail.com>
2023-06-16 12:25:13 -0700

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    '''Create a cursor object to execute SQL queries.'''
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    '''Fetch all the rows returned by the query.'''
+    rows = cur.fetchall()
+    ''' Display the results.'''
+    for row in rows:
+        print(row)
+
+    '''Close the cursor and database connection.'''
+    cur.close()
+    db.close()
