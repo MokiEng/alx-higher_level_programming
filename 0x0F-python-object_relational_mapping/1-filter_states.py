09a0e1274192a01ae9819b723dd4053e31587a91
MokiEng <meconen20@gmail.com>
2023-06-16 11:59:28 -0700

    Object relational mapper

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
-'''
-a python a script that lists all states
-from the database hbtn_0e_0_usa.
-'''
-
+"""
+a script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
 
 
 def list_states(username, password, database):
-    """Connect to MySQL server."""
+    '''Connect to MySQL server.'''
     db = MySQLdb.connect(user=username, passwd=password,
                          db=database, host='localhost', port=3306)
     ''' Create a cursor object to execute SQL queries.'''
     cursor = db.cursor()
 
-    ''' Execute the query to fetch states.'''
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    ''' Execute the query to fetch states starting with 'N'.'''
 
-    ''' Fetch all the rows returned by the query.'''
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    '''Fetch all the rows returned by the query.'''
     rows = cursor.fetchall()
 
-    ''' Display the results.'''
+    '''Display the results.'''
     for row in rows:
         print(row)
 
-    ''' Close the cursor and database connection.'''
+    '''Close the cursor and database connection.'''
+
     cursor.close()
     db.close()
 
 
-''' Entry point of the script.'''
+'''Entry point of the script.'''
 if __name__ == '__main__':
     import sys
 
-    ''' Get the command-line arguments.'''
+    '''Get the command-line arguments.'''
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    ''' Call the function to list states.'''
+    '''Call the function to list states.'''
     list_states(username, password, database)
