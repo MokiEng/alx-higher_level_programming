33868b9313f76fafc929a4f0e24659bcd5597da7
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-17 13:21:50 -0700

    Update relationship_city.py

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -12,6 +12,6 @@ from sqlalchemy.ext.declarative import declarative_base
 class City(Base):
     ''' class that defines each city.'''
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
