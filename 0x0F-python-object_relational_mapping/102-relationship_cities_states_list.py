f817a834c7bb58387c53dbf8e85d8f052c5f3ec1
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-17 13:17:08 -0700

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -12,7 +12,7 @@ from relationship_city import City
 import sys
 
 
-def list_cities(username, password, database):
+if __name__ == "__main__":
     '''Create a SQLAlchemy engine to connect to the MySQL server.'''
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
@@ -28,22 +28,10 @@ def list_cities(username, password, database):
     cities = session.query(City).order_by(City.id).all()
 
     '''Display the cities.'''
-    for city in cities:
-        state_name = city.state.name if city.state else "Unknown"
-        print(f'{city.id}: {city.name} ({state_name})')
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
 
     '''Close the session.'''
     session.close()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print('Usage: python script.py <username> <password> <database>')
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    '''Call the function to list cities.'''
-    list_cities(username, password, database)
