c844ef16a1e9a9e658d512c0c662e2a39aac19f8
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-25 12:48:18 -0700

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,45 +1,22 @@
-#!/usr/bin/node
+!/usr/bin/node
 
 const request = require('request');
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-// Function to get all characters of a Star Wars movie
-function getMovieCharacters(movieId) {
-  const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-  request.get(apiUrl, (err, response, body) => {
-    if (err) {
-      console.error(, err);
-    } else {
-      if (response.statusCode === 200) {
-        const movieData = JSON.parse(body);
-        const characterUrls = movieData.characters;
+request(apiUrl, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
 
-        // Function to fetch character names based on URLs
-        function fetchCharacterName(characterUrl) {
-          request.get(characterUrl, (err, response, body) => {
-            if (err) {
-              console.error(err);
-            } else {
-              if (response.statusCode === 200) {
-                const characterData = JSON.parse(body);
-                console.log(characterData.name);
-              } else {
-                console.error(`Failed to fetch character data. Status code: ${response.statusCode}`);
-              }
-            }
-          });
-        }
-
-        // Fetch and print character names one by one
-        characterUrls.forEach(fetchCharacterName);
-      } else {
-        console.error(`Failed to fetch movie data. Status code: ${response.statusCode}`);
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
     }
   });
-}
-
-// Read the command-line argument (Movie ID)
-const movieId = process.argv[2];
-
-// Call the function to get and print all characters of the Star Wars movie
-getMovieCharacters(movieId);
+}    
