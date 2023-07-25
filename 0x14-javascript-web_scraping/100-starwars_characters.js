da41fb02ac7fcbee457e70b47c676201880eb49f
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-25 12:40:08 -0700

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movie_id = process.argv[2];
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
-req.get(apiUrl + movie_id, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const j of dd) {
-    req.get(j, function (error, response, body1) {
-      if (error) {
-        console.log(error);
-      }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
+const swUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(swUri, function (_err, _res, body) {
+  const characters = JSON.parse(body).characters;
+
+  for (let k = 0; k < characters.length; ++k) {
+    request(characters[k], function (_cErr, _cRes, cBody) {
+      console.log(JSON.parse(cBody).name);
     });
   }
 });
