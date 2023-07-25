0148b1972ac49f807167f5a65a27e3f714405936
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-25 12:07:55 -0700

    Create 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Function to compute the number of completed tasks by user ID
+function countCompletedTasks(apiUrl) {
+  request.get(apiUrl, (err, response, body) => {
+    if (err) {
+      console.error(err);
+    } else {
+      if (response.statusCode === 200) {
+        const todos = JSON.parse(body);
+        const completedTasks = {};
+
+        todos.forEach((todo) => {
+          if (todo.completed) {
+            if (completedTasks[todo.userId]) {
+              completedTasks[todo.userId]++;
+            } else {
+              completedTasks[todo.userId] = 1;
+            }
+          }
+        });
+
+        console.log(completedTasks);
+      } else {
+        console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
+      }
+    }
+  });
+}
+
+// Read the command-line argument (API URL)
+const apiUrl = process.argv[2];
+
+// Call the function to compute the number of completed tasks by user ID
+countCompletedTasks(apiUrl);
