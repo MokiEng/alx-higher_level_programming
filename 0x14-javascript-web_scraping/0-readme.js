4acc54f6fe51ac2e5771478b57c059118255b811
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-25 08:36:04 -0700

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs');
 
-#Function to read and print the content of the file
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
 });
