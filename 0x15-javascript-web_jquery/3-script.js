0fdd957d1836b9bfb34434e792364a419505be7f
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-08-02 12:27:56 -0700

    Add `.red` class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+// Attach a click event handler to the <div> element with id "red_header"
+  $('div#red_header').click(function() {
+    // Add the class "red" to the <header> element
+    $('header').addClass('red');
+  });
+});
