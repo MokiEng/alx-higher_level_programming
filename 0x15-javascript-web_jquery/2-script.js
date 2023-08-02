df7e48b54fe0f91ce21217c20d2902378246c379
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-08-02 12:25:51 -0700

    Create 2-script.js

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Attach a click event handler to the <div> element with id "red_header"
+$('DIV#red_header').click(function () {
+  // Update the text color of the <header> element to red
+  $('HEADER').css('color', '#FF0000');
+});
